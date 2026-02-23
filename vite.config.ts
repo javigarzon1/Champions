8b3c92a0d2193841d19c6d4b86474c5cd02e4551
javier Garzon <javi.garzon@hotmail.com>
2026-02-23 19:37:04 +0000

    f

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import componentTagger from "vite-plugin-component-tagger";
-// https://vitejs.dev/config/
-  export default defineConfig({
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === "development" && componentTagger()
+  ].filter(Boolean),
+
+  define: {
+    __APP_ENV__: JSON.stringify(mode)
+  },
+
   server: {
     host: true,
     port: 5173,
     strictPort: true
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-});
+  }
+}));
